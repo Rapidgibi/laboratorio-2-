8ef18346fe73fd8f7939d4a565bdb4934cf855b0
Rapidgibi <gibi23507@gmail.com>
2026-09-18 16:48:11 -0500

    agregando pagina de productos

--- servidor.js
+++ servidor.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 const productosRoutes = require('./routes/productos');
 
+app.use(express.static('public'));
+
 app.use('/', productosRoutes);
+
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
